58747f83026b99df302643cd23e3fff894257725
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-19 13:52:39 +0100

    icon play on card

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="col text-center" id={props.songInfo.id}>
-      <img className="img-fluid" src={props.songInfo.album.cover_medium} alt="track" />
-      <p>
-        Track:{" "}
-        {props.songInfo.title.length < 16 ? `${props.songInfo.title}` : `${props.songInfo.title.substring(0, 16)}...`}
-        <br />
-        Artist: {props.songInfo.artist.name}
-      </p>
+    <div className="col text-center " id={props.songInfo.id}>
+      <div className="position-relative MyImgWrapper">
+        <img className="img-fluid" src={props.songInfo.album.cover_medium} alt="track" />
+        <i class="bi bi-play-circle-fill fs-3  text-success position-absolute "></i>
+
+        <p>
+          Track:{" "}
+          {props.songInfo.title.length < 16 ? `${props.songInfo.title}` : `${props.songInfo.title.substring(0, 16)}...`}
+          <br />
+          Artist: {props.songInfo.artist.name}
+        </p>
+      </div>
     </div>
   );
 }
